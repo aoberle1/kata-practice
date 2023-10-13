bd47a064373c69e09b602a70ba59c5cd04aa19e7
Adam Oberle <oberle.adam@gmail.com>
2023-10-13 16:19:39 -0500

    completed highest and lowest kata, remember Math.max and Math.min

--- 7-highest-and-lowest.js
+++ 7-highest-and-lowest.js
@@ -1,43 +1,35 @@
+"use strict";
 // In this little assignment you are given a string of space separated numbers, 
 // and have to return the highest and lowest number.
-
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Kata = void 0;
 // Examples
 // highAndLow("1 2 3 4 5");  // return "5 1"
 // highAndLow("1 2 -3 4 5"); // return "5 -3"
 // highAndLow("1 9 3 4 -5"); // return "9 -5"
-
 // Notes
 // All numbers are valid Int32, no need to validate them.
 // There will always be at least one number in the input string.
 // Output string must be two numbers separated by a single space, and highest number is first.
-
-export class Kata {
-    static highAndLow(numbers: string): string {
-        let numbersArray: number[] = numbers.split(" ").map(Number);
-        let highNumber: number = numbersArray[0];
-        let lowNumber: number = numbersArray[0];
-
+var Kata = /** @class */ (function () {
+    function Kata() {
+    }
+    Kata.highAndLow = function (numbers) {
+        var numbersArray = numbers.split(" ").map(Number);
+        var highNumber = numbersArray[0];
+        var lowNumber = numbersArray[0];
         console.log('numbersArray is:', numbersArray);
-
-        for (let i=0; i < numbersArray.length; i++) {
+        for (var i = 0; i < numbersArray.length; i++) {
             if (highNumber < numbersArray[i]) {
                 highNumber = numbersArray[i];
-            } 
+            }
             else if (numbersArray[i] < lowNumber) {
                 lowNumber = numbersArray[i];
             }
         }
-        return `"${highNumber} ${lowNumber}"`
-    }
-}
-
+        return "\"".concat(highNumber, " ").concat(lowNumber, "\"");
+    };
+    return Kata;
+}());
+exports.Kata = Kata;
 console.log(Kata.highAndLow("1 2 -3 4 5"));
-
-//code wars better solution:
-// export class Kata {
-//     static highAndLow(numbers: string): string {    
-//       const numberList = numbers.split(' ').map(Number);   
-//       return `${Math.max(...numberList)} ${Math.min(...numberList)}`;
-//     }
-//   }
-
