7877c103f0fc91f03c4e422d71fe69f0f271d085
Adam Oberle <oberle.adam@gmail.com>
2023-10-13 12:35:50 -0500

    completed string to array kata - remember to add a space to the seperator to split whole words in a string

---
+++ 8-convert-string-to-array.js
@@ -0,0 +1,10 @@
+"use strict";
+// Write a function to split a string and convert it into an array of words.
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.stringToArray = void 0;
+function stringToArray(s) {
+    // code code code
+    return s.split(" ");
+}
+exports.stringToArray = stringToArray;
+console.log(stringToArray("Robin Singh"));
---
+++ 8-convert-string-to-array.ts
@@ -0,0 +1,8 @@
+// Write a function to split a string and convert it into an array of words.
+
+export function stringToArray(s: string): string[] {
+	// code code code
+  return s.split(" ");
+}
+
+console.log(stringToArray("Robin Singh"));
